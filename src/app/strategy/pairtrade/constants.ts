a33735742ef5647bfe65d72e9fd211c3a65883ca
Mridul Agarwal <mridulagarwal@MacBook-Pro.local>
2020-12-13 16:05:08 +0530

    V1.1

---
+++ src/app/strategy/pairtrade/constants.ts
@@ -0,0 +1,99 @@
+export const NiftyBank = [
+    'HDFCBANK',
+    'RBLBANK',
+    'PNB',
+    'ICICIBANK',
+    'BANDHANBNK',
+    'KOTAKBANK',
+    'IDFCFIRSTB',
+    'BANKBARODA',
+    'AXISBANK',
+    'SBIN',
+    'FEDERALBNK',
+    'INDUSINDBK'
+];
+
+
+export const NIFTYAUTO = [
+    'M&M',
+    'BAJAJ-AUTO',
+    'MRF',
+    'TATAMOTORS',
+    'TVSMOTOR',
+    'EXIDEIND',
+    'MOTHERSUMI',
+    'ASHOKLEY',
+    'EICHERMOT',
+    'HEROMOTOCO',
+    'BHARATFORG',
+    'BOSCHLTD',
+    'MARUTI',
+    'BALKRISIND',
+    'AMARAJABAT'
+];
+
+export const NIFTYFS = [
+    'CHOLAFIN',
+    'HDFCBANK',
+    'PFC',
+    'ICICIPRULI',
+    'ICICIGI',
+    'BAJAJHLDNG',
+    'HDFCLIFE',
+    'PEL',
+    'ICICIBANK',
+    'SRTRANSFIN',
+    'HDFC',
+    'BAJAJFINSV',
+    'KOTAKBANK',
+    'SBILIFE',
+    'RECLTD',
+    'BAJFINANCE',
+    'HDFCAMC',
+    'AXISBANK',
+    'SBIN',
+];
+
+export const NIFTYFMCG = [
+    'BRITANNIA',
+    'MCDOWELL-N',
+    'NESTLEIND',
+    'DABUR',
+    'JUBLFOOD',
+    'HINDUNILVR',
+    'MARICO',
+    'COLPAL',
+    'TATACONSUM',
+    'GODREJCP',
+    'PGHH',
+    'ITC',
+    'VBL',
+    'UBL',
+    'EMAMILTD'
+];
+
+export const NIFTYIT = [
+    'INFY',
+    'COFORGE',
+    'WIPRO',
+    'MINDTREE',
+    'TCS',
+    'LTI',
+    'NAUKRI',
+    'TECHM',
+    'HCLTECH',
+    'MPHASIS'
+];
+
+export const NIFTPHARMA = [
+    'DRREDDY',
+    'CADILAHC',
+    'SUNPHARMA',
+    'LUPIN',
+    'DIVISLAB',
+    'AUROPHARMA',
+    'CIPLA',
+    'TORNTPHARM',
+    'BIOCON',
+    'ALKEM'
+];
